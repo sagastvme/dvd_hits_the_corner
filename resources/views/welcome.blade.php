c64f9a99ce45f05d02ccc3a9ffecb4ead21440ae
Eduardo <miltormentas@tutanota.com>
2023-07-17 14:45:10 -0400

    moved image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,8 +6,27 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>DVD Screensaver</title>
+    <script defer>
+        // window.onload=console.log(window.innerHeight, window.innerWidth)
+        function move_icon(){
+            let img = document.getElementById('dvd');
+
+        img.style.position='absolute';
+
+
+            for (let i = 1; i < window.innerWidth -100; i++) {
+                setTimeout(() => {
+                    console.log(i + 'px');
+                    img.style.left = i + 'px'
+                    console.log('hola')
+                }, i * 2);
+
+            }
+        }
+        document.addEventListener('DOMContentLoaded', move_icon);
+    </script>
 </head>
 <body>
-<img width="100px" src="DVD_logo.svg" alt="DVD moving logo">
+<img  id="dvd" width="100px" src="DVD_logo.svg" alt="DVD moving logo">
 </body>
 </html>
